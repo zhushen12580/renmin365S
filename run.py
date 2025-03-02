9e389eb8283e06c4bf179982e4f33d05d7ee437c
zhushen <2270364052@qq.com>
2025-03-02 16:10:35 +0800

    UI设计草稿

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ app.config.from_object(Config)
 db.init_app(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0',port=port)
